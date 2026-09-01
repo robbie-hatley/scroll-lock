7c6135d063700915b46766fc6c2a7ca543767cc8
Robbie Hatley <Hatley.Software@gmail.com>
2026-09-01 16:41:34 -0700

    Create scroll-lock-toggle.sh

---
+++ scroll-lock-toggle.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+pidfile=/usr/local/sbin/scroll-lock/process.pid
+if [[ -f "$pidfile" ]]; then
+    # Process IS running, so turn scroll-lock "off", kill process, and remove process-id file:
+    sudo /usr/local/sbin/scroll-lock/scroll-lock.sh off
+    pid=$(cat "$pidfile")
+    kill "$pid" 2>/dev/null
+    rm -f "$pidfile"
+else
+    # Process is NOT running, so turn scroll-lock "on", launch process, and create process-id file:
+    sudo /usr/local/sbin/scroll-lock/scroll-lock.sh on
+    /home/aragorn/Data/Celephais/rhe-cli/scripts/music/aleatoric.pl &
+    echo $! > "$pidfile"
+fi
